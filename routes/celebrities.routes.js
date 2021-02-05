ac79de6892323ade405a28b27595b6faf44bc4e1
Barbara Diaz <barbarad91@gmail.com>
2021-02-05 20:35:18 +0100

    Display a specified celebrity's details

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,14 +3,21 @@ const router = express.Router()
 const Celebrity = require("../models/Celebrity.model")
 
 // Endpoints
-router.get("/", (req, res, next) => {
+router.get("/", (req, res, next) =>
   Celebrity.find()
+    .select("name")
     .then((allCelebrities) =>
       res.render("celebrities/celebrities-list", { allCelebrities })
     )
-    .catch((err) => {
-      next(err)
-    })
-})
+    .catch((err) => next(err))
+)
+
+router.get("/:id", (req, res, next) =>
+  Celebrity.findById(req.params.id)
+    .then((celebrity) =>
+      res.render("celebrities/celebrity-detail", { celebrity })
+    )
+    .catch((err) => next(err))
+)
 
 module.exports = router
