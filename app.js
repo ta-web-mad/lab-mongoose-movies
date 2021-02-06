ec16904d031688b30c941dfc9b6f080fef38ea71
Barbara Diaz <barbarad91@gmail.com>
2021-02-06 10:54:49 +0100

    Add new celebrity

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require("dotenv").config()
 
 // Database
-require('./configs/mongoose.config')
+require("./configs/mongoose.config")
 
 // Debugger
-require('./configs/debugger.config')
+require("./configs/debugger.config")
 
 // App
-const express = require('express')
+const express = require("express")
 const app = express()
 
 // Configs
-require('./configs/preformatter.config')(app)
-require('./configs/middleware.config')(app)
-require('./configs/views.configs')(app)
-require('./configs/locals.config')(app)
+require("./configs/preformatter.config")(app)
+require("./configs/middleware.config")(app)
+require("./configs/views.configs")(app)
+require("./configs/locals.config")(app)
 
 // Routes index
-require('./routes')(app)
+require("./routes")(app)
 
 module.exports = app
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const Celebrity = require("../models/Celebrity.model")
 
 // Endpoints
+// Celebrities list
 router.get("/", (req, res, next) =>
   Celebrity.find()
     .select("name")
@@ -12,6 +13,46 @@ router.get("/", (req, res, next) =>
     .catch((err) => next(err))
 )
 
+// Create celebrity
+router.get("/new-celebrity", (req, res) =>
+  res.render("celebrities/new-celebrity")
+)
+
+router.post("/new-celebrity", (req, res) => {
+  let { name, occupation, catchPhrase } = req.body
+
+  // Format name
+  let [artistName, artistLastname] = name.split(" ")
+  artistName =
+    artistName[0].toUpperCase() + artistName.substring(1).toLowerCase()
+  artistLastname =
+    artistLastname?.charAt(0).toUpperCase() +
+    artistLastname?.substring(1).toLowerCase()
+  name = artistName.concat(artistLastname ? artistLastname : "")
+
+  //  Format occupation
+  occupation =
+    occupation.charAt(0).toUpperCase() + occupation.substring(1).toLowerCase()
+
+  //Format catch phrase
+  catchPhrase =
+    catchPhrase.charAt(0).toUpperCase() + catchPhrase.substring(1).toLowerCase()
+
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((newCelebrity) => {
+      console.log("url", req.originalUrl)
+      Celebrity.find()
+        .select("name")
+        .then((allCelebrities) =>
+          res.render("celebrities/celebrities-list", { allCelebrities })
+        )
+    })
+    .catch((err) => {
+      res.render("celebrities/new-celebrity")
+    })
+})
+
+// Celebrity detail
 router.get("/:id", (req, res, next) =>
   Celebrity.findById(req.params.id)
     .then((celebrity) =>
