699d441bf3f84313e893c7df9af19fb98f35f6cb
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-02-06 13:13:50 +0100

    Celebrities list view and celebrity info view done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+const Celebrity = require('../models/celebrity.model')
+
 // Endpoints
-router.get('/', (req, res) => res.render('index'))
+router.get('/', (req, res) => {
+    Celebrity
+        .find()
+        .select('name')
+        .then(celebs => res.render('celebrities/index', {celebs}))
+        .catch(err => console.log('Error:', err))
+    
+})
+
+router.get('/:id', (req, res) => {
+    const celebID = req.params.id
+    Celebrity
+        .findById(celebID)
+        .then(celeb => res.render('celebrities/celebrityInfo', celeb))
+        .catch(err => console.log('Error', err))
+})
 
 
 module.exports = router
