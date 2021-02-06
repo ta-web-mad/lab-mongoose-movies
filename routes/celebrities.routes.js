444aa7be1ef297b3be46e5a7f97c355492e29803
Veronica Alcala <veronica.alcala@gmail.com>
2021-02-06 22:20:17 +0100

    iteration 3, done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -5,14 +5,26 @@ const mongoose = require('mongoose')
 
 const Celebrity = require('../model/celebrity.model')
 
-//This is the endpoint
-
+//List
 router.get('/', (req, res) => {
     Celebrity
-    .find()
-    .then(celebrities => res.render('celebrities/index', {celebrities, error: req.query.error}))
-    .catch(err => console.log('ERROR:', err))
+        .find()
+        .select("name")
+        .then(celebrities => res.render('celebrities/index', {celebrities, error: req.query.error}))
+        .catch(err => console.log('ERROR:', err))
 })
+
+//Details
+router.get('/details/:_id', (req, res) => {
+    const celebrity_id = req.params._id
+
+    Celebrity
+        .findById(celebrity_id)
+        .then(celebrity => res.render('celebrities/details', celebrity))
+        .catch(err => console.log('ERROR:', err))
+
+})
+
 
 
 module.exports = router
