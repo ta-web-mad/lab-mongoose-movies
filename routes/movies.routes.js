0281bce0948574f7f2f6b80dbe493c421ff9bcf3
Barbara Diaz <barbarad91@gmail.com>
2021-02-06 17:17:58 +0100

    Display movie details

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -11,4 +11,10 @@ router.get("/", (req, res, next) =>
     .catch((err) => next(err))
 )
 
+//Movie detail
+router.get("/:id", (req, res, next) =>
+  Movie.findById(req.params.id)
+    .then((movie) => res.render("movies/movie-detail", { movie }))
+    .catch((err) => next(err))
+)
 module.exports = router
