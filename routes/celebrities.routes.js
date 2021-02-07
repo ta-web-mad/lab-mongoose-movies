927b9563b1c9f3492724bffee22b88cc6eaa27e3
Veronica Alcala <veronica.alcala@gmail.com>
2021-02-07 15:59:19 +0100

    iterations 4, 5 and bonus 6, done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const mongoose = require('mongoose')
+const { findByIdAndRemove } = require('../model/celebrity.model')
 
 const Celebrity = require('../model/celebrity.model')
 
@@ -25,6 +26,47 @@ router.get('/details/:_id', (req, res) => {
 
 })
 
+//Add
+router.get('/new', (req, res) => res.render('celebrities/new-form'))
 
+router.post('/new', (req, res) => {
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+        .create({ name, occupation, catchPhrase })
+        .then(response => res.redirect(`/celebrities`))
+        .catch(err => console.log('ERROR:', err))
+
+})
+
+//Delete
+router.post('/:_id/delete', (req, res) => {
+    const celebrity_id = req.params._id
+
+    Celebrity
+    .findByIdAndRemove(celebrity_id)
+    .then(celebrity => res.redirect('/celebrities'))
+    .catch(err => console.log('ERROR:', err))
+})
+
+//Editing
+router.get('/edit/:_id', (req, res) => {
+    const celebrity_id = req.params._id
+
+    Celebrity
+    .findById(celebrity_id)
+    .then(celebrity => res.render('celebrities/edit-form', celebrity))
+    .catch(err => console.log('ERROR:', err))
+})
+
+router.post('/edit/:_id', (req, res) => {
+    const {name, occupation, catchPhrase} = req.body
+    const celebrity_id = req.params._id
+
+    Celebrity
+    .findByIdAndUpdate(celebrity_id, {name, occupation, catchPhrase})
+    .then(celebrity => res.redirect(`/celebrities/details/${celebrity._id}`))
+    .catch(err => console.log('ERROR:', err))
+})
 
 module.exports = router
