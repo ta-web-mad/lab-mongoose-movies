d2ae252b264a5bdcf13e7dc5a5aa57ee3a8a5791
Achraf <Achrafarazzouk@gmail.com>
2021-04-18 14:45:15 +0200

    Iteration 2

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -3,6 +3,17 @@ const router = express.Router()
 
 // Endpoints
 router.get('/', (req, res) => res.render('pages/index'))
+const Celeb = require('./../models/celebrity.model')
+
+//Celebs
+
+router.get('/celebrities', (req, res) => {
+
+    Celeb
+        .find()
+        .then(allCelebs => res.render('pages/celebrities/index', { data: allCelebs }))
+        .catch(err => console.log('Error!', err))
+})
 
 
 module.exports = router
