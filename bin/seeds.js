50ad37476ed1824663e73c4f7594a0fef85343d1
Achraf <Achrafarazzouk@gmail.com>
2021-04-18 14:28:23 +0200

    Iteration 1

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,8 +1,40 @@
+const Celebrity = require('../models/celebrity.model')
+
 require('dotenv').config()
 
+const mongoose = require('mongoose')
+
+const Celeb = require('./../models/celebrity.model')
 
 // DB connection
 require('./../config/db.config')
 
 
 // Seed here!
+
+const celebrities = [
+    {
+        name: "Perro Sanchez",
+        occupation: "Politician",
+        catchPhrase: "Ser malos! Buenas noches colegas"
+    },
+    {
+        name: "Sergio Ramos",
+        occupation: "Footballer",
+        catchPhrase: "Unos jugaban al baloncesto, otros al basket..."
+    },
+    {
+        name: "Victor del Diario de Patricia",
+        occupation: "TV celebrity",
+        catchPhrase: "Fumo pa' hacerme el chulo"
+    },
+]
+
+
+Celeb
+    .create(celebrities)
+    .then(res => {
+        console.log("Hola, ya hay famosos");
+        mongoose.connection.close()
+    })
+    .catch(err => console.log('se produjo un error...', err))
