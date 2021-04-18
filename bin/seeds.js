972c5b3181dc850f8adc65dc10a848a4a4a14666
David <davidedson2030@outlook.com>
2021-04-18 18:29:33 +0200

    done first part

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,3 +1,6 @@
+const { Mongoose } = require('mongoose');
+const Celebrity = require('../models/celebrity');
+
 require('dotenv').config()
 
 
@@ -6,3 +9,29 @@ require('./../config/db.config')
 
 
 // Seed here!
+const celebrity = [
+    {
+        name: 'Tom Cruise',
+        occupation: 'Actor',
+        catchPhrase: 'Hasta la vista baby'
+    },
+
+    {
+        name: 'Ronaldinho',
+        occupation: 'Soccer player',
+        catchPhrase: 'Quer namorar comigo?'
+    },
+
+    {
+        name: 'Buzz Lightyear',
+        occupation: 'Astronaut',
+        catchPhrase: 'To infinity... and beyond!'
+    }
+];
+
+Celebrity
+    .create(celebrity)
+    .then(list => {
+        console.log('Celebrity', list)
+    })
+    .catch(err => console.log('Error!', err))
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,60 @@
 const express = require('express')
+const Celebrity = require('../models/celebrity')
 const router = express.Router()
 
 // Endpoints
 router.get('/', (req, res) => res.render('pages/index'))
 
+router.get('/celebrities', (req, res, next) => {
+
+    Celebrity
+        .find()
+        .then(celebrities => {
+            res.render('pages/celebrities/index', { celebrities })
+        })
+        .catch(err => {
+            next()
+            console.log('Error!', err)
+        })
+})
+
+router.get('/celebrities/:celebrities_id', (req, res, next) => {
+
+    const { celebrities_id } = req.params
+
+    Celebrity
+        .findById(celebrities_id)
+        .then(theCelebrity => res.render('pages/celebrities/show', theCelebrity))
+        .catch(err => {
+            next()
+            console.log('Error!', err)
+        })
+})
+
+router.get('/celebrities/new', (req, res) => res.render('pages/celebrities/new'))
+
+router.post('/celebrities/new', (req, res) => {
+
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+        .create({ name, occupation, catchPhrase })
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => {
+            res.redirect('/celebrities/new?msg=Error-Try Again')
+            console.log('Error!', err)
+        })
+})
+
+router.post('/celebrities/:celebrities_id/delete', (req, res, next) => {
+    const { celebrities_id } = req.params
+    Celebrity
+        .findByIdAndRemove(celebrities_id)
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => {
+            next();
+            console.log('Error!', err)
+        })
+})
 
 module.exports = router
