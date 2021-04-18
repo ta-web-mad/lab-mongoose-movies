099f3f40e15017eee130bcf7cd189c7c0584b4e2
Achraf <Achrafarazzouk@gmail.com>
2021-04-18 16:42:00 +0200

    Iteration 4-5

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -7,11 +7,11 @@ router.get('/', (req, res) => {
 
     Celeb
         .find()
-        .then(allCelebs => res.render('pages/celebrities/index', { data: allCelebs }))
+        .then(allCelebs => res.render('pages/celebrities/index', { data: allCelebs, msg: req.query.msg }))
         .catch(err => console.log('Error!', err))
 })
 
-router.get('/:id', (req, res) => {
+router.get('/details/:id', (req, res) => {
     const id = req.params.id
 
     Celeb
@@ -22,6 +22,28 @@ router.get('/:id', (req, res) => {
         })
         .catch(err => console.log('Error!', err))
 
+})
+
+router.get('/new', (req, res) => {
+    res.render('pages/celebrities/new')
+})
+
+router.post('/', (req, res) => {
+    const { name, occupation, phrase } = req.body
+
+    Celeb
+        .create({ name, occupation, phrase })
+        .then(() => res.redirect('/celebrities?msg=Famoso creado'))
+        .catch(err => console.log('Error!', err))
+})
+
+router.post('/delete/:id', (req, res) => {
+    const id = req.params.id
+
+    Celeb
+        .findByIdAndRemove(id)
+        .then(() => res.redirect('/celebrities?msg=Famoso eliminado'))
+        .catch(err => console.log('Error!', err))
 })
 
 module.exports = router
