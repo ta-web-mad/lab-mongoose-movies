7b3300ed5c01fd459edfb56f3c2a8eac1fb88d89
Juanjo Prada <juanjopradaalfaro@gmail.com>
2021-04-19 08:59:07 +0200

    done

--- bin/seeds.js
+++ bin/seeds.js
@@ -4,5 +4,37 @@ require('dotenv').config()
 // DB connection
 require('./../config/db.config')
 
+const mongoose = require('mongoose')
+
+//model requirement
+const Celebrity = require('./../models/celebrity.model')
 
 // Seed here!
+
+const celebrities = [
+    {
+       name: 'George Best',
+           occupation: 'Futbolista',
+           catchPhrase: 'Gasté un montón de dinero en coches, mujeres y alcohol. El resto simplemente lo malgasté'
+    },
+    {
+         name: 'Matias Prats',
+             occupation: 'Presentador',
+             catchPhrase: 'La NASA sufre un recorte de presupuesto, se ve que no están para tirar cohetes'
+    },
+    {
+      name: 'Martin Luther King',
+          occupation: 'Pastor',
+          catchPhrase: 'I have a dream'
+      },
+];
+
+// Code Here
+
+Celebrity
+    .create(celebrities)
+    .then(response => {
+        console.log('Estas son las celebrities!', response)
+        mongoose.connection.close();
+    })
+    .catch(err => console.log('se produjo un error...', err))
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,98 @@
 const express = require('express')
+const Celebrity = require('./../models/celebrity.model')
 const router = express.Router()
+
 
 // Endpoints
 router.get('/', (req, res) => res.render('pages/index'))
+
+// All celebrities list
+
+router.get('/celebrities/index', (req, res) => {
+
+    Celebrity
+        .find()
+        .then(allCelebrities => res.render('pages/celebrities/index', {
+            allCelebrities
+        }))
+        .catch(err => console.log('Error!', err))
+})
+
+// celebrity details
+router.get('/celebrities/:celebrity_id', (req, res, next) => {
+
+    const { celebrity_id }  = req.params
+
+    Celebrity
+        .findById(celebrity_id)
+        .then(celebrityDetails => res.render('pages/celebrities/show', celebrityDetails))
+        .catch(err => {
+            next();
+            return err;
+        })
+})
+
+
+// add new celebrities
+
+router.get('/celebrities/new', (req, res) => res.render('pages/celebrities/new'))
+
+// Add the new celebrity to the database
+
+router.post('/celebrities/new', (req, res) => {
+
+    const {name,occupation,catchPhrase} = req.body
+
+    Celebrity
+        .create({ name,occupation,catchPhrase})
+        .then(() => res.redirect('/celebrities/index'))
+        .catch(err => {res.render("pages/celebrities/new")})
+})
+
+// delete celebrity
+
+
+router.post('/celebrities/:celebrity_id/delete', (req, res, next) => {
+
+    const {celebrity_id } = req.params
+
+    Celebrity
+        .findByIdAndRemove(celebrity_id)
+        .then(() => res.redirect('/celebrities/index'))
+        .catch(err => {
+            next();
+            return err;
+        })
+})
+
+// edit celebrity
+
+router.get('/celebrities/:celebrity_id/edit', (req, res, next) => {
+
+    const {celebrity_id} = req.params
+
+    Celebrity
+        .findById(celebrity_id)
+        .then(celebrity => res.render('pages/celebrities/edit', celebrity))
+        .catch(err => {
+            next();
+            return err;
+        })
+})
+
+router.post('/celebrities/:celebrity_id', (req, res, next) => {
+
+    const { celebrity_id } = req.params
+    const { name,occupation,catchPhrase} = req.body
+
+    Celebrity
+        .findByIdAndUpdate(celebrity_id, { name,occupation,catchPhrase})
+        .then(() => res.redirect('/celebrities/index'))
+        .catch(err => {
+            next();
+            return err;
+        })
+})
 
 
 module.exports = router
