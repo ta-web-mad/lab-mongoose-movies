f8248dbf81bee3f50a7482b9b4e4465b4489e8d3
Guille Muñoz <guillermomunozgonzalez@MacBook-Pro-de-Guillermo.local>
2021-04-19 09:23:59 +0200

    done

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,4 +5,61 @@ require('dotenv').config()
 require('./../config/db.config')
 
 
+const { Mongoose } = require('mongoose')
 // Seed here!
+
+const Celebrity = require('./../models/celebrity')
+
+const celebrities = [
+    {
+        name: "Victorio",
+        occupation: "Actor",
+        catchPhrase: "Just do it",
+    },
+    {
+        name: "El aberroncho",
+        occupation: "Guardian del bosque",
+        catchPhrase: "bijain the musgo",
+    },
+    {
+        name: "Vinicius Jr",
+        occupation: "Jogador du futebol",
+        catchPhrase: "gustosoooo",
+    }
+]
+
+Celebrity
+    .create(celebrities)
+    .then(response => {
+        console.log('estas son las celebridades', response)
+        mongoose.connection.close()
+})
+    .catch(err => console.log('se produjo un error!', err))
+
+const Movie = require('./../models/movie')
+
+const movies = [
+    {
+        title: "Dos rubias de pelo en pecho",
+        genre: "comedia",
+        plot: "policia secreta vestidos de mujer",
+    },
+    {
+        title: "Sexo en Nueva York 2",
+        genre: "Romance",
+        plot: "A mi novia le gusta mucho",
+    },
+    {
+        title: "Ali g",
+        genre: "comedia",
+        plot: "dos palabras Res Pecto",
+    }
+]
+
+Movie
+    .create(movies)
+    .then(response => {
+        console.log('estas son las celebridades', response)
+        mongoose.connection.close()
+    })
+    .catch(err => console.log('se produjo un error!', err))
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,114 @@
+const { response } = require('express')
 const express = require('express')
 const router = express.Router()
+
+const Celebrity = require('../models/celebrity')
+const Movie = require('../models/movie')
 
 // Endpoints
 router.get('/', (req, res) => res.render('pages/index'))
 
+router.get('/celebrities', (req, res) =>{
+    Celebrity
+        .find()
+        .then(response =>{
+            // console.log('estas son las celebrities enrutadas', response)
+            // res.send('vamooooooos')
+            res.render('pages/celebrities/index', { response })
+        })
+        .catch(err => console.log('errorrr!', err ))
+})
+
+router.get('/celebrities/:_id', (req, res) => {
+    const { _id } = req.params
+    console.log('este es el id',_id)
+
+    Celebrity
+        .findById(_id)
+        .then(response => {
+            console.log(response);
+            res.render('pages/celebrities/show', response)
+        })
+        .catch(err => console.log('errorrr!', err))
+})
+
+router.get('/celebrities/new', (req, res) => res.render('pages/celebrities/new'))
+
+
+router.post('/celebrities/new', (req, res) => {
+    const { name, occupation, catchPhrase } = req.body
+    Celebrity
+        .create({ name, occupation, catchPhrase })
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => res.render('pages/celebrities/new'))
+})
+
+router.post('/celebrities/:id/delete', (req, res) =>{
+    const { id } = req.body
+
+    console.log('este es el id que se va a eliminar', id)
+
+    Celebrity
+        .findByIdAndRemove(id)
+        .then(response => res.redirect('/celebrities', response))
+        .catch(err => console.log('errorrr!', err))
+
+
+})
+
+
+
+
+/*-------MOVIES-------*/
+
+
+router.get('/movies', (req, res) => {
+    //console.log('Go to /movies/index.')
+    Movie
+        .find()
+        .then(movies => {
+            //console.log('Movies:', movies )
+            res.render('pages/movies/index', { movies })
+        })
+        .catch(err => console.log('erroorrr!:', err))
+})
+
+router.get('/movies/:_id', (req, res) => {
+    const { _id } = req.params
+    console.log('este es el id', _id)
+
+    Movie
+        .findById(_id)
+        .then(response => {
+            console.log(response);
+            res.render('pages/movies/show', response)
+        })
+        .catch(err => console.log('errorrr!', err))
+})
+
+
+router.get('/movies/new', (req, res) => res.render('pages/movies/new'))
+
+router.post('/movies/new', (req, res) => {
+
+    const { title, genre, plot } = req.body
+
+    Movie
+        .create({ title, genre, plot })
+        .then(() => res.redirect('/movies'))
+        .catch(err => res.render('pages/movies/new'))
+})
+router.post('/movies/:id/delete', (req, res) => {
+    const { id } = req.params
+
+    console.log('este es el id que se va a eliminar', id)
+
+    Movie
+        .findByIdAndRemove(id)
+        .then(response => res.redirect('/movies', response))
+        .catch(err => console.log('errorrr!', err))
+
+
+})
 
 module.exports = router
